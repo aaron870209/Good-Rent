a61b00a5b2abc87ffa30fec94f1349252b609ec4
Aaron <kairong0209@gmail.com>
2022-06-16 14:11:07 +0800

    insert data to mongoDB

--- crawler_591.py
+++ crawler_591.py
@@ -1,7 +1,3 @@
-from fake_useragent import UserAgent
-import requests
-import pymysql.cursors
-import time
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -11,6 +7,8 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 import threading
 
+
+
 load_dotenv()
 client = MongoClient('localhost:27017',
                      username=os.getenv('mongo_user'),
@@ -18,97 +16,115 @@ client = MongoClient('localhost:27017',
                      # authSource='stylish_data_engineering',
                      # authMechanism='SCRAM-SHA-1'
                      )
-db = client['admin']
-mycol = db["test_col"]
+db = client['crawler_data']
+mycol = db["raw_data"]
 
 
-def crawler(region):
-    start = time.perf_counter()
+def crawler(region,mycol):
+    from datetime import date
+    import time
     driver = webdriver.Chrome()
     driver.get(f"https://rent.591.com.tw/?region={region}")
-    # time.sleep(3)
-    price_list = WebDriverWait(driver,10).until(
-        EC.presence_of_all_elements_located((By.CLASS_NAME, "item-price-text"))
-    )
     total_house = WebDriverWait(driver,10).until(
         EC.presence_of_element_located((By.CLASS_NAME, "switch-amount"))
     ).find_element(By.TAG_NAME, "span")
-    title_list = WebDriverWait(driver,10).until(
-        EC.presence_of_all_elements_located((By.CLASS_NAME,"item-title"))
-    )
-    item_detail_list = WebDriverWait(driver,10).until(
-        EC.presence_of_all_elements_located((By.CLASS_NAME,"item-style"))
-    )
-    address_list = WebDriverWait(driver,10).until(
-        EC.presence_of_all_elements_located((By.CLASS_NAME,"item-area"))
-    )
-    for i in range(1,17):
-        driver.execute_script(f"window.scrollTo(0,500*{i})")
-        time.sleep(0.5)
-    img_crawler_list = WebDriverWait(driver,10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "list-container-content"))
-    ).find_elements(By.CLASS_NAME,"carousel-list")
     total_house_to_string = total_house.text
     total_house = int(total_house_to_string.replace(",", ""))
-    price = [x.text for x in price_list]
-    title = [x.text for x in title_list]
-    item_detail = [x.text for x in item_detail_list]
-    address = [x.find_element(By.TAG_NAME,"span").text for x in address_list]
-    img = [x.find_element(By.TAG_NAME,"img").get_attribute("src") for x in img_crawler_list]
-    page = int(total_house/30)
-    print("Total=", total_house)
-    print("price=", price)
-    print("title=", title)
-    print("detail=", item_detail)
-    print("address=", address)
-    print("img=",img)
-    print(page)
-    driver.close()
-    for i in range(1, 5):
-        first_row = i*30
-        total_row = total_house
-        driver = webdriver.Chrome()
-        driver.get(f"https://rent.591.com.tw/?region={region}&firstRow={first_row}&totalRows={total_row}")
-        price_list = WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.CLASS_NAME, "item-price-text"))
-        )
-        title_list = WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.CLASS_NAME, "item-title"))
-        )
-        item_detail_list = WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.CLASS_NAME, "item-style"))
-        )
-        address_list = WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.CLASS_NAME, "item-area"))
-        )
-        for i in range(1, 17):
-            driver.execute_script(f"window.scrollTo(0,500*{i})")
-            time.sleep(0.5)
-        img_crawler_list = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "list-container-content"))
-        ).find_elements(By.CLASS_NAME, "carousel-list")
-        price_1 = [x.text for x in price_list]
-        price.extend(price_1)
-        title_1 = [x.text for x in title_list]
-        title.extend(title_1)
-        item_detail_1 = [x.text for x in item_detail_list]
-        item_detail.extend(item_detail_1)
-        address_1 = [x.text for x in address_list]
-        address.extend(address_1)
-        img_1 = [x.find_element(By.TAG_NAME, "img").get_attribute("src") for x in img_crawler_list]
-        img.extend(img_1)
-        driver.close()
-    print("price=", price)
-    print("title=", title)
-    print("detail=", item_detail)
-    print("address=", address)
-    print("img=",img)
-    end = time.perf_counter()
-    print("time=",end-start)
+    last_page = int(total_house / 30)
+    price = []
+    title = []
+    item_detail = []
+    address = []
+    img = []
+    for i in range(1, last_page+1):
+        try:
+            price_list = WebDriverWait(driver,10).until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME, "item-price-text"))
+            )
+
+            title_list = WebDriverWait(driver,10).until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME,"item-title"))
+            )
+            item_detail_list = WebDriverWait(driver,10).until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME,"item-style"))
+            )
+            address_list = WebDriverWait(driver,10).until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME,"item-area"))
+            )
+            for i in range(1,17):
+                driver.execute_script(f"window.scrollTo(0,500*{i})")
+                time.sleep(0.5)
+            img_crawler_list = WebDriverWait(driver,10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "list-container-content"))
+            ).find_elements(By.CLASS_NAME,"carousel-list")
+            price_1 = [x.text for x in price_list]
+            price.extend(price_1)
+            title_1 = [x.text for x in title_list]
+            title.extend(title_1)
+            item_detail_1 = [x.text for x in item_detail_list]
+            item_detail.extend(item_detail_1)
+            address_1 = [x.find_element(By.TAG_NAME,"span").text for x in address_list]
+            address.extend(address_1)
+            img_1 = [x.find_element(By.TAG_NAME,"img").get_attribute("src") for x in img_crawler_list]
+            img.extend(img_1)
+            if i < last_page:
+                driver.find_element(By.CLASS_NAME,'pageNext').click()
+            else:
+                driver.close()
+        except:
+            driver.refresh()
+            price_list = WebDriverWait(driver, 10).until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME, "item-price-text"))
+            )
+
+            title_list = WebDriverWait(driver, 10).until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME, "item-title"))
+            )
+            item_detail_list = WebDriverWait(driver, 10).until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME, "item-style"))
+            )
+            address_list = WebDriverWait(driver, 10).until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME, "item-area"))
+            )
+            for i in range(1, 17):
+                driver.execute_script(f"window.scrollTo(0,500*{i})")
+                time.sleep(0.5)
+            img_crawler_list = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "list-container-content"))
+            ).find_elements(By.CLASS_NAME, "carousel-list")
+            price_1 = [x.text for x in price_list]
+            price.extend(price_1)
+            title_1 = [x.text for x in title_list]
+            title.extend(title_1)
+            item_detail_1 = [x.text for x in item_detail_list]
+            item_detail.extend(item_detail_1)
+            address_1 = [x.find_element(By.TAG_NAME, "span").text for x in address_list]
+            address.extend(address_1)
+            img_1 = [x.find_element(By.TAG_NAME, "img").get_attribute("src") for x in img_crawler_list]
+            img.extend(img_1)
+            if i < last_page:
+                driver.find_element(By.CLASS_NAME, 'pageNext').click()
+            else:
+                driver.close()
+    if region == 1:
+        city="臺北市"
+    else:
+        city = "新北市"
+    dict = {"title":title,"price":price,"item_detail":item_detail,"address":address,"img":img,"source":"591","region":city}
+    time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+    today = date.today()
+    dict_to_mongo = {"data":dict,"create_time":time,"create_date":today}
+    mycol.insert_one(dict_to_mongo)
+
+# def do_sth(driver, class_name):
+#     total_house = WebDriverWait(driver,10).until(
+#         EC.presence_of_element_located((By.CLASS_NAME, "switch-amount"))
+#     ).find_element(By.TAG_NAME, "span")
+#     return total_house
 
 
-t1 = threading.Thread(target=crawler ,args=(1,))
-t2 = threading.Thread(target=crawler, args=(3,))
+t1 = threading.Thread(target=crawler, args=(1, mycol))
+t2 = threading.Thread(target=crawler, args=(3, mycol))
 t1.start()
 t2.start()
 t1.join()
--- crawler_lewu.py
+++ crawler_lewu.py
@@ -3,19 +3,36 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
 import re
 import threading
 
+
+
+
+load_dotenv()
+client = MongoClient('localhost:27017',
+                     username=os.getenv('mongo_user'),
+                     password=os.getenv('mongo_password'),
+                     # authSource='stylish_data_engineering',
+                     # authMechanism='SCRAM-SHA-1'
+                     )
+print(os.getenv('mongo_user'))
+db = client['crawler_data']
+mycol = db["raw_data"]
 Taipei = "eJyrVkrOLKlUsopWMlCK1VFKySwuyEkE8pVyMotLlHSU8pOyMvNSQPJBIPni1MSi5AwQF6wNKFJanJqcn5IKEjIHqrcAYksgNjQAEsZKsbUArxsbyQ"
 New_Taipei = "eJyrVkrOLKlUsopWMlKK1VFKySwuyEkE8pVyMotLlHSU8pOyMvNSQPJBIPni1MSi5AwQF6wNKFJanJqcn5IKEjIHqrcAYksgNjQAEsZKsbUAr8kbyw"
 
 
-def crawler(region):
+def crawler(region,mycol):
+    from datetime import date
+    import time
     driver = webdriver.Chrome()
     driver.get(f"https://www.rakuya.com.tw/search/rent_search/index?con={region}&upd=1")
     total_pages = driver.find_element(By.CLASS_NAME,"pages").text
     last_page = int(total_pages.split(' ')[3])
-    print("last_page=",last_page)
     price = []
     title = []
     item_detail = []
@@ -50,9 +67,9 @@ def crawler(region):
             else:
                 driver.close()
         except:
-            # for i in range(10):
-            driver.refresh()
-            # time.sleep(5)
+            for i in range(3):
+                driver.refresh()
+                time.sleep(3)
             product_list = WebDriverWait(driver, 10).until(
                 EC.presence_of_all_elements_located((By.CLASS_NAME, "obj-item.clearfix"))
             )
@@ -86,10 +103,19 @@ def crawler(region):
                 driver.find_element(By.XPATH, f"/html/body/div[8]/div/div[1]/nav/ul/li[{6}]/a").click()
             else:
                 driver.close()
+    if region == "eJyrVkrOLKlUsopWMlCK1VFKySwuyEkE8pVyMotLlHSU8pOyMvNSQPJBIPni1MSi5AwQF6wNKFJanJqcn5IKEjIHqrcAYksgNjQAEsZKsbUArxsbyQ":
+        city = "臺北市"
+    else:
+        city = "新北市"
+    today = date.today()
+    dict = {"title":title,"price":price,"item_detail":item_detail,"address":address,"img":img,"source":"樂屋網","region":city}
+    time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+    dict_to_mongo = {"data":dict, "create_time":time, "create_date":today}
+    mycol.insert_one(dict_to_mongo)
 
 
-t1 = threading.Thread(target=crawler, args=(Taipei,))
-t2 = threading.Thread(target=crawler, args=(New_Taipei,))
+t1 = threading.Thread(target=crawler, args=(Taipei, mycol))
+t2 = threading.Thread(target=crawler, args=(New_Taipei, mycol))
 t1.start()
 t2.start()
 t1.join()
