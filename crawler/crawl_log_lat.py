eb2151026286dfd333aaacf71af203f2b83e0f6e
Aaron <kairong0209@gmail.com>
2022-07-06 09:47:14 +0800

    [crawler][python] useing Nominatim to change address to lat lon

--- crawler/crawl_log_lat.py
+++ crawler/crawl_log_lat.py
@@ -8,6 +8,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from fake_useragent import UserAgent
 from MySQL import MySQL
 import threading
+from geopy.geocoders import Nominatim
 
 
 def get_coordinate(addr, house_id):
@@ -15,7 +16,6 @@ def get_coordinate(addr, house_id):
         import time
         chrome_options = Options()
         ua = UserAgent().random
-        # chrome_options.add_argument('--proxy-server=http://202.20.16.82:10152')
         chrome_options.add_argument('--headless')
         chrome_options.add_argument("user-agent={}".format(ua))
         browser = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=chrome_options)
@@ -24,16 +24,13 @@ def get_coordinate(addr, house_id):
         search.clear()
         search.send_keys(addr)
         browser.find_element_by_xpath("/html/body/form/div[10]/div[2]/img[2]").click()
-        # time.sleep(5)
         time.sleep(3)
         iframe = browser.find_element_by_class_name('winfoIframe')
         print(iframe)
         browser.switch_to.frame(iframe)
-        # time.sleep(3)
         coor_btn = WebDriverWait(browser, 10).until(
                     EC.presence_of_element_located((By.XPATH, "/html/body/form/div[4]/table/tbody/tr[3]/td/table/tbody/tr/td[2]"))
                 )
-        # coor_btn = browser.find_element_by_xpath("/html/body/form/div[4]/table/tbody/tr[3]/td/table/tbody/tr/td[2]")
         coor_btn.click()
         coor = browser.find_element_by_xpath("/html/body/form/div[5]/table/tbody/tr[2]/td")
         coor = coor.text.strip().split(" ")
@@ -56,7 +53,6 @@ def get_coordinate(addr, house_id):
         coor_btn = WebDriverWait(browser, 10).until(
                     EC.presence_of_element_located((By.XPATH, "/html/body/form/div[4]/table/tbody/tr[3]/td/table/tbody/tr/td[2]"))
                 )
-        # coor_btn = browser.find_element_by_xpath("/html/body/form/div[4]/table/tbody/tr[3]/td/table/tbody/tr/td[2]")
         coor_btn.click()
         coor = browser.find_element_by_xpath("/html/body/form/div[5]/table/tbody/tr[2]/td")
         coor = coor.text.strip().split(" ")
@@ -65,6 +61,26 @@ def get_coordinate(addr, house_id):
         browser.quit()
         dict = {"log":log,"lat":lat,"id":house_id}
         MySQL.update_house_log_lat(dict)
+
+
+def change_address_to_latlon(address,house_id):
+    geolocator = Nominatim(user_agent="GoodRent")
+    location = geolocator.geocode(address)
+    try:
+        longitude = location.longitude
+        latitude = location.latitude
+        dict = {"log": longitude, "lat": latitude, "id": house_id}
+        MySQL.update_house_log_lat(dict)
+    except AttributeError:
+        try:
+            address = address.replace("台北市","").replace("新北市","")
+            location = geolocator.geocode(address)
+            longitude = location.longitude
+            latitude = location.latitude
+            dict = {"log": longitude, "lat": latitude, "id": house_id}
+            MySQL.update_house_log_lat(dict)
+        except AttributeError:
+            get_coordinate(address, house_id)
 
 
 def get_data_from_DB():
@@ -76,7 +92,8 @@ def get_data_from_DB():
             city = "新北市"
         house_id = house["house_id"]
         address = city + house["address"]
-        get_coordinate(address,house_id)
+        print(house_id)
+        change_address_to_latlon(address,house_id)
 
 
 if __name__ == "__main__":
