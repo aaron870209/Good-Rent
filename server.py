d77cde659546568ee06ba423c5133a3487d32347
Aaron <kairong0209@gmail.com>
2022-07-14 13:39:47 +0800

    [server][python] add error handling

--- server.py
+++ server.py
@@ -8,82 +8,109 @@ app = Flask(__name__)
 @app.route("/")
 def home_page(paging=0,tag=0):
     data = []
-    paging = int(request.args.get("paging",paging))
-    tag = int(request.args.get("tag",tag))
-    if tag == 0:
-        for house in MySQL.get_all_info_from_house(paging):
-            data_dict = {"id":house["house_id"],"title":house["title"],"price":house["price"],"address":house["address"],"img":house["img"],
-                    "size":house["size"],"floor":house["floor"]+"F","longitude":house["longitude"],"latitude":house["latitude"],
-                    "city":house["city"],"type":house["house_type"],"paging":paging,"tag":tag}
-            data.append(data_dict)
-        return render_template("home_page.html",data=data)
-    else:
-        for house in MySQL.get_filter_info_from_house(paging,tag):
-            data_dict = {"id": house["house_id"], "title": house["title"], "price": house["price"],
-                         "address": house["address"], "img": house["img"],
-                         "size": house["size"], "floor": house["floor"] + "F", "longitude": house["longitude"],
-                         "latitude": house["latitude"],
-                         "city": house["city"], "type": house["house_type"], "paging": paging, "tag": tag}
-            data.append(data_dict)
-        return render_template("home_page.html", data=data)
+    try:
+        paging = int(request.args.get("paging",paging))
+        tag = int(request.args.get("tag",tag))
+        if tag == 0:
+            house_list,total = MySQL.get_all_info_from_house(paging)
+            total = total["count(*)"]
+            limit_page = int(total/15)
+            if paging > limit_page:
+                return render_template("404.html")
+            else:
+                for house in house_list:
+                    data_dict = {"id":house["house_id"],"title":house["title"],"price":house["price"],"address":house["address"],"img":house["img"],
+                            "size":house["size"],"floor":house["floor"]+"F","longitude":house["longitude"],"latitude":house["latitude"],
+                            "city":house["city"],"type":house["house_type"],"paging":paging,"tag":tag}
+                    data.append(data_dict)
+                return render_template("home_page.html", data=data, total=total)
+        elif tag == 1 or tag == 2 or tag == 3:
+            house_list,total = MySQL.get_filter_info_from_house(paging,tag)
+            total = total["count(*)"]
+            limit_page = int(total / 15)
+            if paging > limit_page:
+                return render_template("404.html")
+            else:
+                for house in house_list:
+                    data_dict = {"id": house["house_id"], "title": house["title"], "price": house["price"],
+                                 "address": house["address"], "img": house["img"],
+                                 "size": house["size"], "floor": house["floor"] + "F", "longitude": house["longitude"],
+                                 "latitude": house["latitude"],
+                                 "city": house["city"], "type": house["house_type"], "paging": paging, "tag": tag}
+                    data.append(data_dict)
+                return render_template("home_page.html", data=data,total=total)
+        else:
+            return render_template("404.html")
+    except ValueError:
+        return render_template("404.html")
 
 
 @app.route("/search", methods=['POST'])
 def search_page():
     tag = json.loads(request.data)
     print(tag)
-    data = MySQL.search_house(tag["data"], tag["page"])
-    dict = {"data": data, "page": tag["page"]}
+    data,total = MySQL.search_house(tag["data"], tag["page"])
+    total = total["count(*)"]
+    dict = {"data": data, "page": tag["page"],"total":total}
     return dict
 
 
 @app.route("/detail", methods=["GET"])
 def detail_page():
     id = request.args.get('id')
-    detail = MySQL.get_house_detail_by_id(int(id))
-    truck_position = MySQL.get_truck_house_distance(id)
-    school_position = MySQL.get_school_house_distance(id)
-    print(truck_position)
-    truck_position_list = []
-    print(truck_position_list)
-    m = folium.Map(location=[detail["latitude"],detail["longitude"]],zoom_start=16)
-    print(detail["title"])
-    iframe = folium.IFrame(f'<b>{detail["title"]}</b>')
-    popup = folium.Popup(iframe, min_width=300,max_width=300,min_height=40,max_height=80)
-    m.add_child(folium.Marker(location=[detail["latitude"],detail["longitude"]],popup=popup,
-                              icon=folium.Icon(icon='home',color='green')))
-    for truck in truck_position:
-        truck_id = truck["truck_spot_id"]
-        position = MySQL.get_truck_lon_lat_by_id(truck_id)
-        longitude = position["longitude"]
-        latitude = position["latitude"]
-        list = [latitude,longitude]
-        m.add_child(folium.Marker(location=list, opacity=0.8,
-                                  icon=folium.Icon(icon='truck', color='blue', prefix='fa')))
-    print(detail["house_type"])
-    if detail["house_type"] == "整層住家":
-        show_school = True
+    print(id)
+    if id == None:
+        return render_template('404.html')
     else:
-        show_school = False
-    feature_group = folium.FeatureGroup(name='學校', show=show_school)
-    for school in school_position:
-        school_id = school["school_id"]
-        position =MySQL.get_school_lon_lat_by_id(school_id)
-        longitude = position["longitude"]
-        latitude = position["latitude"]
-        list = [latitude, longitude]
-        iframe = folium.IFrame(f'<b>{position["name"]}</b>')
-        popup = folium.Popup(iframe, min_width=300, max_width=300, min_height=40, max_height=80)
-        folium.Marker(location=list, opacity=0.8,popup=popup,
-                      icon=folium.Icon(icon='graduation-cap', color='red', prefix='fa')).add_to(feature_group)
-    feature_group.add_to(m)
-    folium.LayerControl().add_to(m)
-    m = m._repr_html_()
-    if "電梯" in detail["tag"]:
-        elevator = "有"
-    else:
-        elevator = "無"
-    return render_template("main_page.html", detail=detail, map=m,elevator=elevator)
+        try:
+            detail = MySQL.get_house_detail_by_id(int(id))
+            truck_position = MySQL.get_truck_house_distance(id)
+            school_position = MySQL.get_school_house_distance(id)
+            print(truck_position)
+            truck_position_list = []
+            print(truck_position_list)
+            m = folium.Map(location=[detail["latitude"],detail["longitude"]],zoom_start=16)
+            print(detail["title"])
+            iframe = folium.IFrame(f'<b>{detail["title"]}</b>')
+            popup = folium.Popup(iframe, min_width=300,max_width=300,min_height=40,max_height=80)
+            m.add_child(folium.Marker(location=[detail["latitude"],detail["longitude"]],popup=popup,
+                                      icon=folium.Icon(icon='home',color='green')))
+            for truck in truck_position:
+                truck_id = truck["truck_spot_id"]
+                position = MySQL.get_truck_lon_lat_by_id(truck_id)
+                longitude = position["longitude"]
+                latitude = position["latitude"]
+                list = [latitude,longitude]
+                m.add_child(folium.Marker(location=list, opacity=0.8,
+                                          icon=folium.Icon(icon='truck', color='blue', prefix='fa')))
+            print(detail["house_type"])
+            if detail["house_type"] == "整層住家":
+                show_school = True
+            else:
+                show_school = False
+            feature_group = folium.FeatureGroup(name='學校', show=show_school)
+            for school in school_position:
+                school_id = school["school_id"]
+                position =MySQL.get_school_lon_lat_by_id(school_id)
+                longitude = position["longitude"]
+                latitude = position["latitude"]
+                list = [latitude, longitude]
+                iframe = folium.IFrame(f'<b>{position["name"]}</b>')
+                popup = folium.Popup(iframe, min_width=300, max_width=300, min_height=40, max_height=80)
+                folium.Marker(location=list, opacity=0.8,popup=popup,
+                              icon=folium.Icon(icon='graduation-cap', color='red', prefix='fa')).add_to(feature_group)
+            feature_group.add_to(m)
+            folium.LayerControl().add_to(m)
+            m = m._repr_html_()
+            if "電梯" in detail["tag"]:
+                elevator = "有"
+            else:
+                elevator = "無"
+            return render_template("main_page.html", detail=detail, map=m, elevator=elevator)
+        except TypeError:
+            return render_template("404.html")
+        except ValueError:
+            return render_template("404.html")
 
 
 if __name__ == '__main__':
