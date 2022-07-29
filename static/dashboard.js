75e66a6913e250e47329b5cb18e2b9d3903e712e
Aaron <kairong0209@gmail.com>
2022-07-29 13:42:37 +0800

    [refactor][Python] remove the console.log

--- static/dashboard.js
+++ static/dashboard.js
@@ -3,95 +3,89 @@ let chart1Id = 'chart1';
 let chart2Id = 'chart2';
 let chart3Id = 'chart3';
 
-// CHART DATA
-// -----------------------------
-
-
-// CHART CONFIG
-// -----------------------------
-
-
-// Chart 2
-function draw_realtime(data){
-    let sales = [[data.new_data[data.new_data.length-1]], [data.Taipei_591[0]],
-    [data.New_Taipei_591[0]], [data.Taipei_lewu[0]],
-    [data.New_Taipei_lewu[0]]];
-    let chart2Data = {
-      type: 'hbar',
-      globals: {
-        fontFamily: 'Poppins',
-      },
-      backgroundColor: 'transparent',
-      title: {
-        text: 'Real Time',
-        align: 'left',
-        fontColor: 'var(--lightGray)',
-        fontWeight: 'normal',
-        padding: '16px',
-      },
-      plot: {
-        tooltip: {
-          visible: false,
-        },
-        barSpace: '32px',
-        barWidth: '12px',
-        borderWidth: '0px',
-      },
-      plotarea: {
-        margin: '40px 56px 16px 56px',
-      },
-      scaleY: {
-        visible: false,
-      },
-      scaleX: {
-        labels: ["資料量"],
-        lineWidth: '0px',
-        tick: {
-          visible: true,
-        },
-      },
-      scaleX2: {
-        values: [
-          sales[4],
-          sales[3],
-          sales[2],
-          sales[1],
-          sales[0],
-        ],
-        lineWidth: '0px',
-        tick: {
-          visible: false,
-        },
-      },
-      series: [
-        {
-          values: sales[0],
-          backgroundColor: 'var(--yellow)',
-        },
-        {
-          values: sales[1],
-          backgroundColor: '#FFFF6F',
-        },
-        {
-          values: sales[2],
-          backgroundColor: '#FFFFAA',
-        },
-        {
-          values: sales[3],
-          backgroundColor: '#53FF53',
-        },
-        {
-          values: sales[4],
-          backgroundColor: '#A6FFA6',
-        },
-      ],
-    };
-    zingchart.render({
-      id: chart2Id,
-      data: chart2Data,
-      height: '300px',
-      width: '100%',
-    });
+function draw_realtime(data) {
+	let sales = [
+		[data.new_data[data.new_data.length - 1]],
+		[data.Taipei_591[0]],
+		[data.New_Taipei_591[0]],
+		[data.Taipei_lewu[0]],
+		[data.New_Taipei_lewu[0]]
+	];
+	let chart2Data = {
+		type: 'hbar',
+		globals: {
+			fontFamily: 'Poppins',
+		},
+		backgroundColor: 'transparent',
+		title: {
+			text: 'Real Time',
+			align: 'left',
+			fontColor: 'var(--lightGray)',
+			fontWeight: 'normal',
+			padding: '16px',
+		},
+		plot: {
+			tooltip: {
+				visible: false,
+			},
+			barSpace: '32px',
+			barWidth: '12px',
+			borderWidth: '0px',
+		},
+		plotarea: {
+			margin: '40px 56px 16px 56px',
+		},
+		scaleY: {
+			visible: false,
+		},
+		scaleX: {
+			labels: ["資料量"],
+			lineWidth: '0px',
+			tick: {
+				visible: true,
+			},
+		},
+		scaleX2: {
+			values: [
+				sales[4],
+				sales[3],
+				sales[2],
+				sales[1],
+				sales[0],
+			],
+			lineWidth: '0px',
+			tick: {
+				visible: false,
+			},
+		},
+		series: [{
+				values: sales[0],
+				backgroundColor: 'var(--yellow)',
+			},
+			{
+				values: sales[1],
+				backgroundColor: '#FFFF6F',
+			},
+			{
+				values: sales[2],
+				backgroundColor: '#FFFFAA',
+			},
+			{
+				values: sales[3],
+				backgroundColor: '#53FF53',
+			},
+			{
+				values: sales[4],
+				backgroundColor: '#A6FFA6',
+			},
+		],
+	};
+	zingchart.render({
+		id: chart2Id,
+		data: chart2Data,
+		height: '300px',
+		width: '100%',
+	});
 
 
 }
@@ -99,141 +93,135 @@ function draw_realtime(data){
 
 
 
-function draw_line(data){
-    // Chart 3
-    let chart3Data = {
-      type: 'line',
-      globals: {
-        fontFamily: 'Poppins',
-      },
-      backgroundColor: 'transparent',
-      scaleX: {
-        labels: data.date.reverse(),
-        tick: {
-          visible: false,
-        },
-      },
-      scaleY: {
-        values: '0: 1000',
-        guide: {
-          lineStyle: 'solid',
-          lineColor: 'var(--lightGray)',
-        },
-        lineWidth: '0px',
-        tick: {
-          visible: false,
-        },
-      },
-      series: [
-        {
-          values: data.new_data.reverse(),
-          lineColor: 'var(--yellow)',
-          lineWidth: '5px',
-          marker: {
-            backgroundColor: 'var(--lightPurple)',
-            borderColor: 'var(--yellow)',
-            borderWidth: '3px',
-            size: 8,
-          },
-        },
-      ],
-};
-
-// Chart 3
-zingchart.render({
-  id: chart1Id,
-  data: chart3Data,
-  height: '250px',
-  width: '800px',
-});
+function draw_line(data) {
+	let chart3Data = {
+		type: 'line',
+		globals: {
+			fontFamily: 'Poppins',
+		},
+		backgroundColor: 'transparent',
+		scaleX: {
+			labels: data.date.reverse(),
+			tick: {
+				visible: false,
+			},
+		},
+		scaleY: {
+			values: '0: 1000',
+			guide: {
+				lineStyle: 'solid',
+				lineColor: 'var(--lightGray)',
+			},
+			lineWidth: '0px',
+			tick: {
+				visible: false,
+			},
+		},
+		series: [{
+			values: data.new_data.reverse(),
+			lineColor: 'var(--yellow)',
+			lineWidth: '5px',
+			marker: {
+				backgroundColor: 'var(--lightPurple)',
+				borderColor: 'var(--yellow)',
+				borderWidth: '3px',
+				size: 8,
+			},
+		}, ],
+	};
+	zingchart.render({
+		id: chart1Id,
+		data: chart3Data,
+		height: '250px',
+		width: '800px',
+	});
 }
 
 
 
 
-function draw_bar(data){
-    var trace1 = {
-      x: data.date,
-      y: data.Taipei_591,
-      name: '591_Taipei',
-      type: 'bar'
-    };
+function draw_bar(data) {
+	var trace1 = {
+		x: data.date,
+		y: data.Taipei_591,
+		name: '591_Taipei',
+		type: 'bar'
+	};
 
-    var trace2 = {
-      x: data.date,
-      y: data.New_Taipei_591,
-      name: '591_New_Taipei',
-      type: 'bar',
-    };
-    var trace3 = {
-      x: data.date,
-      y: data.Taipei_lewu,
-      name: 'lewu_Taipei',
-      type: 'bar'
-    };
-    var trace4 = {
-      x: data.date,
-      y: data.New_Taipei_lewu,
-      name: 'lewu_New_Taipei',
-      type: 'bar'
-    };
+	var trace2 = {
+		x: data.date,
+		y: data.New_Taipei_591,
+		name: '591_New_Taipei',
+		type: 'bar',
+	};
+	var trace3 = {
+		x: data.date,
+		y: data.Taipei_lewu,
+		name: 'lewu_Taipei',
+		type: 'bar'
+	};
+	var trace4 = {
+		x: data.date,
+		y: data.New_Taipei_lewu,
+		name: 'lewu_New_Taipei',
+		type: 'bar'
+	};
 
-    var data = [trace1, trace2, trace3, trace4];
+	var data = [trace1, trace2, trace3, trace4];
 
-    var layout = {barmode: 'group',
-                    paper_bgcolor: "#2d2d45",
-                    plot_bgcolor: "#2d2d45",
-                    colorway: ['#FFFF6F','#FFFFAA','#53FF53','#A6FFA6'],
-                    xaxis: {tickfont: {
-                        size: 14,
-                        color: 'white'
-                    }
-                    },
-                    yaxis: {
-                        range: [0, 13000],
-                        title: '資料筆數',
-                        titlefont: {
-                        size: 16,
-                        color: 'white'
-                        },
-                        tickfont:{
-                        size:14,
-                        color: 'white'
-                        }
-                        },
-                    legend:{
-                        font:{
-                            color:"white"
-                        }
-                    }
-                    };
+	var layout = {
+		barmode: 'group',
+		paper_bgcolor: "#2d2d45",
+		plot_bgcolor: "#2d2d45",
+		colorway: ['#FFFF6F', '#FFFFAA', '#53FF53', '#A6FFA6'],
+		xaxis: {
+			tickfont: {
+				size: 14,
+				color: 'white'
+			}
+		},
+		yaxis: {
+			range: [0, 13000],
+			title: '資料筆數',
+			titlefont: {
+				size: 16,
+				color: 'white'
+			},
+			tickfont: {
+				size: 14,
+				color: 'white'
+			}
+		},
+		legend: {
+			font: {
+				color: "white"
+			}
+		}
+	};
 
-    Plotly.newPlot('chart3', data, layout);
+	Plotly.newPlot('chart3', data, layout);
 }
 
 
 
 
 
-function ajax(){
-var xmlhttp = new XMLHttpRequest();
-    var url = "/dashboard_data";
-    xmlhttp.onreadystatechange = function (){
-        if (this.readyState == 4 && this.status == 200) {
-            let data = this.response
-            console.log("total_data="+data.date)
-            console.log(data.Taipei_591)
-            draw_line(data)
-            draw_bar(data)
-            draw_realtime(data)
-        };
-    };
+function ajax() {
+	var xmlhttp = new XMLHttpRequest();
+	var url = "/dashboard_data";
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			let data = this.response
+			draw_line(data)
+			draw_bar(data)
+			draw_realtime(data)
+		};
+	};
 
 
-    xmlhttp.open("POST", url, true);
-    xmlhttp.setRequestHeader('Content-Type','application/x-www-form-urlencode');
-    xmlhttp.responseType = 'json';
-    xmlhttp.send(JSON.stringify({
-    }));
+	xmlhttp.open("POST", url, true);
+	xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencode');
+	xmlhttp.responseType = 'json';
+	xmlhttp.send(JSON.stringify({}));
 }
 ajax()
--- static/home_page.js
+++ static/home_page.js
@@ -2,240 +2,222 @@
 mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
+	scrollFunction()
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		mybutton.style.display = "block";
+	} else {
+		mybutton.style.display = "none";
+	}
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-function show_detail(tag){
-    if (tag === 1){
-        x = document.getElementById("tag1_detail")
-        x.style.display = "block"
-    }else if(tag === 2){
-        x = document.getElementById("tag2_detail")
-        x.style.display = "block"
-    }else{
-        x = document.getElementById("tag3_detail")
-        x.style.display = "block"
-    }
+function show_detail(tag) {
+	if (tag === 1) {
+		x = document.getElementById("tag1_detail")
+		x.style.display = "block"
+	} else if (tag === 2) {
+		x = document.getElementById("tag2_detail")
+		x.style.display = "block"
+	} else {
+		x = document.getElementById("tag3_detail")
+		x.style.display = "block"
+	}
 }
 
-function hide_detail(tag){
-    if (tag ===1){
-        x = document.getElementById("tag1_detail")
-        x.style.display = "none"
-    }else if(tag === 2){
-        x = document.getElementById("tag2_detail")
-        x.style.display = "none"
-    }else{
-        x = document.getElementById("tag3_detail")
-        x.style.display = "none"
-    }
+function hide_detail(tag) {
+	if (tag === 1) {
+		x = document.getElementById("tag1_detail")
+		x.style.display = "none"
+	} else if (tag === 2) {
+		x = document.getElementById("tag2_detail")
+		x.style.display = "none"
+	} else {
+		x = document.getElementById("tag3_detail")
+		x.style.display = "none"
+	}
 
 }
 
-function show_bar(){
-    x = document.getElementById("menu")
-    y = document.getElementById("dropdownMenuButton1")
-    if (x.style.display === "none"){
-        x.style.display = "block";
-        y.style.marginRight = "28%";
-    }else{
-        x.style.display = "none";
-        y.style.marginRight = "11px";
-    }
+function show_bar() {
+	x = document.getElementById("menu")
+	y = document.getElementById("dropdownMenuButton1")
+	if (x.style.display === "none") {
+		x.style.display = "block";
+		y.style.marginRight = "28%";
+	} else {
+		x.style.display = "none";
+		y.style.marginRight = "11px";
+	}
 }
 
 
 function show_Taipei() {
-  var x = document.getElementById("Taipei_region");
-  var y = document.getElementById("New_Taipei_region")
-  if (x.style.display === "none") {
-    y.style.display = "none";
-    x.style.display = "block";
-  }
+	var x = document.getElementById("Taipei_region");
+	var y = document.getElementById("New_Taipei_region")
+	if (x.style.display === "none") {
+		y.style.display = "none";
+		x.style.display = "block";
+	}
 }
 
 
 function show_New_Taipei() {
-  var x = document.getElementById("New_Taipei_region");
-  var y = document.getElementById("Taipei_region");
-  if (x.style.display === "none") {
-    y.style.display = "none";
-    x.style.display = "block";
-  }
+	var x = document.getElementById("New_Taipei_region");
+	var y = document.getElementById("Taipei_region");
+	if (x.style.display === "none") {
+		y.style.display = "none";
+		x.style.display = "block";
+	}
 }
 
 
-function search(){
-    var data_dict = {}
-    var formEl_taipei = document.querySelectorAll('input[name="台北市地區"]');
-    var checkboxEls_taipei = [];
-    console.log(formEl_taipei)
-    for (var i=0;i<formEl_taipei.length;i++){
-        console.log(formEl_taipei[i].checked)
-        console.log(formEl_taipei[i].value)
-        if(formEl_taipei[i].checked === true){
-        checkboxEls_taipei.push(formEl_taipei[i].getAttribute('value'))
-    }
-    }
-    var formEl_new_taipei = document.querySelectorAll('input[name="新北市地區"]');
-    var checkboxEls_new_taipei = [];
-    console.log(formEl_new_taipei)
-    for (var i=0;i<formEl_new_taipei.length;i++){
-        console.log(formEl_new_taipei[i].checked)
-        console.log(formEl_new_taipei[i].value)
-        if(formEl_new_taipei[i].checked === true){
-        checkboxEls_new_taipei.push(formEl_new_taipei[i].getAttribute('value'))
-    }
-    }
-    var formEl_type = document.querySelectorAll('input[name="house_type"]');
-    var checkboxEls_type = [];
-    console.log(formEl_type)
-    for (var i=0;i<formEl_type.length;i++){
-        console.log(formEl_type[i].checked)
-        console.log(formEl_type[i].value)
-        if(formEl_type[i].checked === true){
-        checkboxEls_type.push(formEl_type[i].getAttribute('value'))
-    }
-    }
+function search() {
+	var data_dict = {}
+	var formEl_taipei = document.querySelectorAll('input[name="台北市地區"]');
+	var checkboxEls_taipei = [];
+	for (var i = 0; i < formEl_taipei.length; i++) {
+		if (formEl_taipei[i].checked === true) {
+			checkboxEls_taipei.push(formEl_taipei[i].getAttribute('value'))
+		}
+	}
+	var formEl_new_taipei = document.querySelectorAll('input[name="新北市地區"]');
+	var checkboxEls_new_taipei = [];
+	for (var i = 0; i < formEl_new_taipei.length; i++) {
+		if (formEl_new_taipei[i].checked === true) {
+			checkboxEls_new_taipei.push(formEl_new_taipei[i].getAttribute('value'))
+		}
+	}
+	var formEl_type = document.querySelectorAll('input[name="house_type"]');
+	var checkboxEls_type = [];
+	for (var i = 0; i < formEl_type.length; i++) {
+		if (formEl_type[i].checked === true) {
+			checkboxEls_type.push(formEl_type[i].getAttribute('value'))
+		}
+	}
 
-    var formEl_rent = document.querySelectorAll('input[name="house_rent"]');
-    var checkboxEls_rent = [];
-    console.log(formEl_rent)
-    for (var i=0;i<formEl_rent.length;i++){
-        console.log(formEl_rent[i].checked)
-        console.log(formEl_rent[i].value)
-        if(formEl_rent[i].checked === true){
-        checkboxEls_rent.push(formEl_rent[i].getAttribute('value'))
-    }
-    }
+	var formEl_rent = document.querySelectorAll('input[name="house_rent"]');
+	var checkboxEls_rent = [];
+	for (var i = 0; i < formEl_rent.length; i++) {
+		if (formEl_rent[i].checked === true) {
+			checkboxEls_rent.push(formEl_rent[i].getAttribute('value'))
+		}
+	}
 
-    data_dict["taipei"]=checkboxEls_taipei
-    data_dict["new_taipei"]=checkboxEls_new_taipei
-    data_dict["type"]=checkboxEls_type
-    data_dict["rent"]=checkboxEls_rent
-    console.log(data_dict)
-    return data_dict
+	data_dict["taipei"] = checkboxEls_taipei
+	data_dict["new_taipei"] = checkboxEls_new_taipei
+	data_dict["type"] = checkboxEls_type
+	data_dict["rent"] = checkboxEls_rent
+	return data_dict
 }
 
 
-function item(data){
-    console.log(data)
-    var data1 = data["data"]
-    for(var i = 0; i < data1.length; i++){
-        console.log(data1[i].title)
-        const div = document.createElement("div")
-        div.className = "create_div"
-        const link = document.createElement("a")
-        link.href = "./detail?id="+data1[i].house_id
-        div.appendChild(link)
-        const img = document.createElement("img");
-        img.className = "img";
-        img.style.width="20%";
-        img.src = data1[i].img;
-        link.appendChild(img);
-        const title = document.createElement("div");
-        title.className = "title";
-        title.innerHTML = '<h2>'+data1[i].title+'</h2>';
-        link.appendChild(title);
-        const address = document.createElement("div");
-        address.className = "address";
-        address.innerHTML = '<h3>'+data1[i].address+'</h3>'
-        link.appendChild(address)
-        const type = document.createElement("div");
-        type.className = "house_type"
-        type.innerHTML = '<h3>'+data1[i].house_type+'</h3>'
-        link.appendChild(type)
-        const detail = document.createElement("div");
-        detail.className = "detail";
-        detail.innerHTML = '<h3>'+data1[i].size+' | '+data1[i].floor+'F</h3>'
-        link.appendChild(detail)
-        const price = document.createElement("div");
-        price.className = "price";
-        price.innerHTML = '<p>'+data1[i].price+'元/月</p>';
-        link.appendChild(price);
-        const clear = document.createElement("div")
-        clear.className = "clear"
-        clear.innerHTML = '<hr class="downer_line2">'
-        link.appendChild(clear)
-        var recommendedProduct = document.getElementById("data");
-        recommendedProduct.appendChild(div);
-                    }
-    var page = data.page
-    console.log(page)
-    var previous_page = document.getElementById("previous_page")
-    previous_page.innerHTML = ''
-    if (page == 0){
-    previous_page.innerHTML = '<a id="上一頁1" class="page-link" onclick="page_control(2)">Previous</a>'
-    }else{
-    previous_page.innerHTML = '<a id="上一頁1" class="page-link" onclick="search_ajax('+(page-1)+')">Previous</a>'
-    }
-    var next_page = document.getElementById("next_page")
-    let total = data.total
-    console.log(total)
-    var limit_page = parseInt(total/15)
-    console.log("總頁"+limit_page)
-    if (page == limit_page){
-        next_page.innerHTML = ''
-        next_page.innerHTML = '<a id="下一頁1" class="page-link" onclick="page_control(1)">next</a>'
-    }else{
-        var next_page = document.getElementById("next_page")
-        next_page.innerHTML = ''
-        next_page.innerHTML = '<a id="下一頁1" class="page-link" onclick="search_ajax('+(page+1)+')">next</a>'
-}
+function item(data) {
+	var data1 = data["data"]
+	for (var i = 0; i < data1.length; i++) {
+		const div = document.createElement("div")
+		div.className = "create_div"
+		const link = document.createElement("a")
+		link.href = "./detail?id=" + data1[i].house_id
+		div.appendChild(link)
+		const img = document.createElement("img");
+		img.className = "img";
+		img.style.width = "20%";
+		img.src = data1[i].img;
+		link.appendChild(img);
+		const title = document.createElement("div");
+		title.className = "title";
+		title.innerHTML = '<h2>' + data1[i].title + '</h2>';
+		link.appendChild(title);
+		const address = document.createElement("div");
+		address.className = "address";
+		address.innerHTML = '<h3>' + data1[i].address + '</h3>'
+		link.appendChild(address)
+		const type = document.createElement("div");
+		type.className = "house_type"
+		type.innerHTML = '<h3>' + data1[i].house_type + '</h3>'
+		link.appendChild(type)
+		const detail = document.createElement("div");
+		detail.className = "detail";
+		detail.innerHTML = '<h3>' + data1[i].size + ' | ' + data1[i].floor + 'F</h3>'
+		link.appendChild(detail)
+		const price = document.createElement("div");
+		price.className = "price";
+		price.innerHTML = '<p>' + data1[i].price + '元/月</p>';
+		link.appendChild(price);
+		const clear = document.createElement("div")
+		clear.className = "clear"
+		clear.innerHTML = '<hr class="downer_line2">'
+		link.appendChild(clear)
+		var recommendedProduct = document.getElementById("data");
+		recommendedProduct.appendChild(div);
+	}
+	var page = data.page
+	var previous_page = document.getElementById("previous_page")
+	previous_page.innerHTML = ''
+	if (page == 0) {
+		previous_page.innerHTML = '<a id="上一頁1" class="page-link" onclick="page_control(2)">Previous</a>'
+	} else {
+		previous_page.innerHTML = '<a id="上一頁1" class="page-link" onclick="search_ajax(' + (page - 1) + ')">Previous</a>'
+	}
+	var next_page = document.getElementById("next_page")
+	let total = data.total
+	var limit_page = parseInt(total / 15)
+	if (page == limit_page) {
+		next_page.innerHTML = ''
+		next_page.innerHTML = '<a id="下一頁1" class="page-link" onclick="page_control(1)">next</a>'
+	} else {
+		var next_page = document.getElementById("next_page")
+		next_page.innerHTML = ''
+		next_page.innerHTML = '<a id="下一頁1" class="page-link" onclick="search_ajax(' + (page + 1) + ')">next</a>'
+	}
 }
 
 
-function page_control(tag){
-    if(tag==1){
-        alert("這是最後一頁囉！！")
-    }else{
-        alert("這是第一頁囉！！")
-    }
+function page_control(tag) {
+	if (tag == 1) {
+		alert("這是最後一頁囉！！")
+	} else {
+		alert("這是第一頁囉！！")
+	}
 }
 
 
 
 
-function search_ajax(page){
-    var x = document.getElementById("home_page_paging");
-    var y = document.getElementById("tag_page_paging")
-    x.style.display = "none";
-    y.style.display = "block";
-    let data= search()
-    console.log(data)
-    var xmlhttp = new XMLHttpRequest();
-    var url = "/search";
-    xmlhttp.onreadystatechange = function (){
-        if (this.readyState == 4 && this.status == 200) {
-            let data = this.response
-            console.log("total_data="+data)
-            document.documentElement.scrollTop = 0;
-            document.getElementById('data').innerHTML = ''
-            item(data)
-            document.getElementById('total').innerHTML = ''
-            document.getElementById('total').innerHTML = '<h1>為您找到 '+data["total"]+' 間適合的房子</h1>'
-        };
-    };
+function search_ajax(page) {
+	var x = document.getElementById("home_page_paging");
+	var y = document.getElementById("tag_page_paging")
+	x.style.display = "none";
+	y.style.display = "block";
+	let data = search()
+	var xmlhttp = new XMLHttpRequest();
+	var url = "/search";
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			let data = this.response
+			document.documentElement.scrollTop = 0;
+			document.getElementById('data').innerHTML = ''
+			item(data)
+			document.getElementById('total').innerHTML = ''
+			document.getElementById('total').innerHTML = '<h1>為您找到 ' + data["total"] + ' 間適合的房子</h1>'
+		};
+	};
 
 
-    xmlhttp.open("POST", url, true);
-    xmlhttp.setRequestHeader('Content-Type','application/x-www-form-urlencode');
-    xmlhttp.responseType = 'json';
-    xmlhttp.send(JSON.stringify({
-        "data": data,"page":page
-    }));
+	xmlhttp.open("POST", url, true);
+	xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencode');
+	xmlhttp.responseType = 'json';
+	xmlhttp.send(JSON.stringify({
+		"data": data,
+		"page": page
+	}));
 }
-
